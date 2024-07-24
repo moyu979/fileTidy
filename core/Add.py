d42a6afabe291823d7b3a1a5d6b54386b1257bb5
moyu979 <moyu979@gmail.com>
2024-07-24 17:43:26 +0800

    finish add func

--- core/Add.py
+++ core/Add.py
@@ -21,29 +21,32 @@ class Add:
         self.processManage=None
 
     @call_decorator
-    def __call__(self) -> os.Any:
-        self.para_convert
+    def __call__(self):
+        self.para_convert()
         for root,dir,files in os.walk(self.data_storage_path):
             for file in files:
                 path=os.path.join(root,file)
                 hash=getAHash(path)
                 self.write_db(hash,path,file)
                 self.processManage.update(path)
+        self.conn.commit()
+        self.conn.close()
 
     def para_convert(self):
-        self.addTime=fileTime
+        self.addTime=fileTime()
         self.conn=sqlite3.connect(os.path.join(self.work_path,"files.db"))
         self.cur=self.conn.cursor()
         self.processManage=ProcessManage(self.data_storage_path)
         #如果提供了磁盘信息，可以根据磁盘信息推断出校验组信息
         if self.storagePhysical!=None:
-            res=self.cur.execute("SELECT * FROM physicalStorage where volumeName=?",(self.storagePhysical,)).fetchall()
+            res=self.cur.execute("SELECT * FROM physicalStorage where volumeName=?",(self.storagePhysical,)).fetchall()[0]
+            
             self.storagePhysical=res[0]
-            res=self.cur.execute("SELECT * FROM storageStructure where subid=?",(self.storagePhysical,)).fetchall()
+            res=self.cur.execute("SELECT * FROM storageStructure where subid=?",(self.storagePhysical,)).fetchall()[0]
             storageVirtual=res[0]
 
             if self.storageVirtual!=None:
-                res=self.cur.execute("SELECT * FROM virtualStorage where volumeName=?",(self.storageVirtual,)).fetchall()
+                res=self.cur.execute("SELECT * FROM virtualStorage where volumeName=?",(self.storageVirtual,)).fetchall()[0]
                 if res[0]==storageVirtual:
                     self.storageVirtual=storageVirtual
                 else:
@@ -53,10 +56,21 @@ class Add:
         else:
             if self.storageVirtual==None:
                 self.storageVirtual="referToDownloadVolumn"
-            res=self.cur.execute("SELECT * FROM virtualStorage where volumeName=?",(self.storageVirtual)).fetchall()
+            res=self.cur.execute("SELECT * FROM virtualStorage where volumeName=?",(self.storageVirtual,)).fetchall()[0]
             self.storageVirtual=res[0]
             self.storagePhysical=0
 
     def write_db(self,hash,path,name):
-        self.cur.execute("INSERT INTO files (Md5,addTime,fromPath,nowPath,nowName,storageVirtual,storagePhysical) VALUES (?,?,?,?,?,?)",(hash,self.addTime,path,path,name,self.storageVirtual,self.storagePhysical))
+        size=get_size(path)
+        self.cur.execute("INSERT INTO files (Md5,addTime,fromPath,nowPath,nowName,storageVirtual,storagePhysical,size) VALUES (?,?,?,?,?,?,?,?)",(hash,self.addTime,path,path,name,self.storageVirtual,self.storagePhysical,size))
 
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dbpath",default="./db",help="dir path to store database")
+    parser.add_argument("--datapath",default=None,help="file path to add")
+    parser.add_argument("--dataStorage",default=None,help="volume to store files")
+    parser.add_argument("--datadisk",default=None,help="disk to store files")
+    args = parser.parse_args()
+    add=Add(args)
+    add()
--- core/Init.sql
+++ core/Init.sql
@@ -1,11 +1,14 @@
 CREATE TABLE files(
     Md5 TEXT,
+    size TEXT,
     addTime TEXT,
     fromPath TEXT,
     nowPath TEXT,
     nowName TEXT,
     storageVirtual INTEGER DEFAULT 0,
-    storagePhysical INTEGER DEFAULT 0
+    storagePhysical INTEGER DEFAULT 0,
+    state TEXT DEFAULT 'healthy',
+    info TEXT DEFAULT NULL
 );
 
 CREATE TABLE virtualStorage(
@@ -13,24 +16,30 @@ CREATE TABLE virtualStorage(
     addTime TEXT,
     lastCheck TEXT,
     volumeName TEXT UNIQUE NOT NULL,
-    needAll INTEGER NOT NULL DEFAULT 1
+    healthy TEXT DEFAULT 'healthy',
+    info TEXT DEFAULT NULL,
+    needAll INTEGER DEFAULT 1
 );
 
 CREATE TABLE physicalStorage(
     id INTEGER PRIMARY KEY,
     addTime TEXT,
     lastCheck TEXT,
-    diskName TEXT UNIQUE NOT NULL
+    diskName TEXT UNIQUE NOT NULL,
+    healthy TEXT DEFAULT 'healthy',
+    info TEXT DEFAULT NULL,
+    diskID TEXT
+    
 );
 
 CREATE TABLE storageStructure(
     superid INTEGER,
     subid INTEGER,
     addTime TEXT,
-    explaination TEXT DEFAULT NULL,
+    info TEXT DEFAULT NULL,
     PRIMARY KEY (superid,subid)
 );
 
-INSERT INTO physicalStorage VALUES (0,'0000-00-00 00:00','0000-00-00 00:00','referToAllDisk');
-INSERT INTO virtualStorage VALUES (0,'0000-00-00 00:00','0000-00-00 00:00','referToDownloadVolumn');
-INSERT INTO storageStructure VALUES (0,0,'0000-00-00 00:00',1,'addByInitProcess');
+INSERT INTO physicalStorage (id,addTime,lastCheck,diskName,diskID) VALUES (0,'0000-00-00 00:00','0000-00-00 00:00','referToAllDisk','0000000000000000');
+INSERT INTO virtualStorage (id,addTime,lastCheck,volumeName)  VALUES (0,'0000-00-00 00:00','0000-00-00 00:00','referToDownloadVolumn');
+INSERT INTO storageStructure VALUES (0,0,'0000-00-00 00:00','addByInitProcess');
