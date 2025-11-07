f3fc6e94e7d03a9f5779f67b91552f29ce06ef0a
moyu979 <moyu979@gmail.com>
2025-11-08 01:36:37 +0800

    完成了device的框架设计

--- component/device/deviceFactory.py
+++ component/device/deviceFactory.py
@@ -2,9 +2,9 @@
 设备工厂，用于创建和管理设备实例,同时提供对外接口
 """
 
-from component.device.HDD import HDD
-from component.device.SSD import SSD
-from component.device.Tape import Tape
+from component.device.drivers.HDD import HDD
+from component.device.drivers.SSD import SSD
+from component.device.drivers.Tape import Tape
 
 class DeviceFactory:
     def __init__(self):
